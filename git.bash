d6bc3634e6a8e646526ac64cb9db59fdb97dd9be
notbryant <notbryant@gmail.com>
2014-03-10 04:05:04 -0600

    Added gitcontrib

--- git.bash
+++ git.bash
@@ -3,3 +3,5 @@ alias gpot="git push origin -f --tags"
 alias git-mergetest="git merge --no-commit --no-ff"
 alias railsdate='date -u +"%Y%m%d%H%M%S"'
 alias gitpush='git push origin $(git rev-parse --abbrev-ref HEAD)'
+#http://stackoverflow.com/questions/4589731/git-blame-statistics
+alias gitcontrib="git ls-tree --name-only -z -r HEAD | xargs -0 -n1 git blame --line-porcelain | grep '^author ' | sed -e 's/^author //;' | sed 's/notbryant/Jon Petraglia/g;' | sed 's/MrQweex/Jon Petraglia/g;' | sort | uniq -c | sort -nr"
